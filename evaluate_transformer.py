d094804c3651ee984cf83b030354b1dc4ee7ddf7
s-broda <s.a.broda@uva.nl>
2019-12-13 17:06:49 +0100

    add transformer

---
+++ evaluate_transformer.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def evaluate(inp_sentence):
+  start_token = [tokenizer_de.vocab_size]
+  end_token = [tokenizer_de.vocab_size + 1]
+  
+  # inp sentence is portuguese, hence adding the start and end token
+  inp_sentence = start_token + tokenizer_de.encode(inp_sentence) + end_token
+  encoder_input = tf.expand_dims(inp_sentence, 0)
+  
+  # as the target is english, the first word to the transformer should be the
+  # english start token.
+  decoder_input = [tokenizer_en.vocab_size]
+  output = tf.expand_dims(decoder_input, 0)
+    
+  for i in range(MAX_LENGTH):
+    enc_padding_mask, combined_mask, dec_padding_mask = create_masks(
+        encoder_input, output)
+  
+    # predictions.shape == (batch_size, seq_len, vocab_size)
+    predictions, attention_weights = transformer(encoder_input, 
+                                                 output,
+                                                 False,
+                                                 enc_padding_mask,
+                                                 combined_mask,
+                                                 dec_padding_mask)
+    
+    # select the last word from the seq_len dimension
+    predictions = predictions[: ,-1:, :]  # (batch_size, 1, vocab_size)
+
+    predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+    
+    # return the result if the predicted_id is equal to the end token
+    if predicted_id == tokenizer_en.vocab_size+1:
+      return tf.squeeze(output, axis=0), attention_weights
+    
+    # concatentate the predicted_id to the output which is given to the decoder
+    # as its input.
+    output = tf.concat([output, predicted_id], axis=-1)
+
+  return tf.squeeze(output, axis=0), attention_weights
+
+def translate(sentence, plot=''):
+  result, attention_weights = evaluate(sentence)
+  
+  predicted_sentence = tokenizer_en.decode([i for i in result 
+                                            if i < tokenizer_en.vocab_size])  
+
+  print('Input: {}'.format(sentence))
+  print('Predicted translation: {}'.format(predicted_sentence))
+  
+  if plot:
+    plot_attention_weights(attention_weights, sentence, result, plot)
+    
